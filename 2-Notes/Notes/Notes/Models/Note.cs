ca17da00d656d1c176df3104d583930e78909979
Eduardo Cardoso Fernandes Neto <ex_mate@hotmail.com>
2020-04-28 13:22:54 -0300

    Exercise: Database

---
+++ 2-Notes/Notes/Notes/Models/Note.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using SQLite;
+
+namespace Notes.Models
+{
+    class Note
+    {
+        [PrimaryKey, AutoIncrement]
+        public string Filename { get; set; }
+        public string Text { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
